40d8bfb85a180a4770b60c0f355d99ca1c879f43
JackTorrence <jacktorrence420@gmx.com>
2020-12-11 19:14:48 +0000

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import lunr from "lunr"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lunrang';
+
+  ngOnInit(): void {
+
+    var documents = [{
+      "name": "Lunr",
+      "text": "Like Solr, but much smaller, and not as bright."
+    }, {
+      "name": "React",
+      "text": "A JavaScript library for building user interfaces."
+    }, {
+      "name": "Lodash",
+      "text": "A modern JavaScript utility library delivering modularity, performance & extras."
+    }]
+
+    var idx = lunr(function () {
+      this.ref('name')
+      this.field('text')
+    
+      documents.forEach(function (doc) {
+        this.add(doc)
+      }, this)
+    })
+    debugger;
+    var results = idx.search("bright");
+    var n =1;
+  }
+
 }
